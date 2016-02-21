587d3ce6c6ce7bacd897cde107129033222e97a2
davidkretch <david.kretch@gmail.com>
2016-02-21 12:25:07 -0500

    Parallel processing of bootstrap replicates

--- src/boot.cpp
+++ src/boot.cpp
@@ -1,27 +1,45 @@
 #include "boot.h"
 
+// TODO: clean up interface to output matrices
+
 namespace bootrlm {
 
 // bootstrap class
 Boot::Boot(const Data& data, Estimator* estimator, const arma::umat& rep)
-  : data(data), n(data.n), p(data.p), rep(rep), r(rep.n_cols) {
+  : data(data), n(data.n), p(data.p), estimator(estimator), rep(rep), r(rep.n_cols) {
 
   coef.set_size(p, r);
   fitted.set_size(n, r);
   resid.set_size(n, r);
   scale.set_size(1, r);
 
-  arma::uvec indices;
-
-  Data resamp;
+  ProcessReplicate process_replicate(data, estimator, rep, coef, fitted, resid, scale);
 
   // for each replicate, construct the dataset then estimate
-  for (int i = 0; i < r; i++) {
+  RcppParallel::parallelFor(0, r, process_replicate);
+
+}
+
+Boot::ProcessReplicate::ProcessReplicate(const Data& data,
+                                         Estimator* estimator,
+                                         const arma::umat& rep,
+                                         arma::mat& coef,
+                                         arma::mat& fitted,
+                                         arma::mat& resid,
+                                         arma::mat& scale)
+  : data(data), estimator(estimator), rep(rep), coef(coef), fitted(fitted),
+    resid(resid), scale(scale) {};
+
+void Boot::ProcessReplicate::operator()(std::size_t begin, std::size_t end) {
+  arma::uvec indices;
+  Data replicate;
+
+  for (std::size_t i = begin; i < end; ++i) {
     indices = rep.col(i);
-    resamp.x = data.x.rows(indices);
-    resamp.y = data.y.elem(indices);
-    (*estimator)(resamp, coef.colptr(i), fitted.colptr(i), resid.colptr(i),
-                 scale.colptr(i));
+    replicate.x = data.x.rows(indices);
+    replicate.y = data.y.elem(indices);
+    (*estimator)(replicate, coef.colptr(i), fitted.colptr(i),
+     resid.colptr(i), scale.colptr(i));
   }
 }
 
--- src/boot.h
+++ src/boot.h
@@ -1,6 +1,7 @@
 #ifndef BOOT_H
 #define BOOT_H
 #include <RcppArmadillo.h>
+#include <RcppParallel.h>
 #include "data.h"
 #include "estimator.h"
 
@@ -11,6 +12,7 @@ public:
   const Data data;
   int n;
   int p;
+  Estimator* estimator;
   const arma::umat rep;
   int r;
   arma::mat coef;
@@ -19,6 +21,24 @@ public:
   arma::mat scale;
 
   Boot(const Data&, Estimator*, const arma::umat&);
+
+private:
+  struct ProcessReplicate : public RcppParallel::Worker {
+
+    // need to pass class members to struct, as they're not visible (in C++98)
+    ProcessReplicate(const Data&, Estimator*, const arma::umat&,
+                     arma::mat&, arma::mat&, arma::mat&, arma::mat&);
+
+    void operator()(std::size_t begin, std::size_t end);
+
+    const Data& data;
+    Estimator* estimator;
+    const arma::umat& rep;
+    arma::mat& coef;
+    arma::mat& fitted;
+    arma::mat& resid;
+    arma::mat& scale;
+  };
 };
 
 } // namespace bootrlm
